8bff6f5b50936b32fd96f9ece162c14eb7b6bb4d
ezekieljimlowell <ezekiel@aptonworks.com>
2023-04-17 15:08:59 +0530

    variable hoisting in js

---
+++ JS BASICS 15.04.2023/variableHoisting.js
@@ -0,0 +1,49 @@
+//const x; caught SyntaxError: Missing initializer in const declaration
+//console.log(a); Uncaught ReferenceError: Cannot access 'a' before initialization 
+//let a;
+//console.log(b) //riableHoisting.js:4 Uncaught ReferenceError: b is not defined
+//"use strict"; //riableHoisting.js:13 Uncaught ReferenceError: b is not defined
+console.log(a) // variable a hoisted with undefined
+var a;
+c = 20;
+console.log(c, "c")
+if(true) {
+    var c = 1;
+    c = 5;
+}
+b = 10;
+console.log(b, "b")
+//console.log(d) //caught ReferenceError: d is not defined
+function summa() {
+    var d;
+    console.log(d)
+}
+summa()
+if(true) {
+    //console.log(e) //caught ReferenceError: Cannot access 'e' before initialization
+    let e;
+    console.log(e) //undefined
+}
+//console.log(e) //caught ReferenceError: d is not defined
+//console.log(f) //caught ReferenceError: Cannot access 'f' before initialization
+const f = "hello"
+console.log(f)
+console.log(g)
+var g = "tell";
+console.log(g)
+g = "why"
+console.log(g)
+const h = "twwdaw"
+h = 3 //caught TypeError: Assignment to constant variable.
+
+
+
+
+
+
+
+
+
+
+
+
